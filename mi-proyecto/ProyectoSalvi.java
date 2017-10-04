0129207f66a47121e20cf4ce377db994a892f725
Salvador Rebollo Benítez <salvirbcontacto@gmail.com>
2017-10-04 10:57:35 +0200

    Update ProyectoSalvi.java

--- mi-proyecto/ProyectoSalvi.java
+++ mi-proyecto/ProyectoSalvi.java
@@ -1,7 +1,4 @@
-/* Ejercicio 9: Escribe un programa que pinte por pantalla alguna escena - el campo, la
-habitación de una casa, un aula, etc. - o algún objeto animado o inanimado
-- un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
-caracteres como *, +, <, #, @, etc. ¡Échale imaginación!
+/* Piano
  */
  
 
