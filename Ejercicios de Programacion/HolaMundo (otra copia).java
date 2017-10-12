732a38bfbef05adec6f42191298296ce7d9a527b
Salvador Rebollo Benítez <salvirbcontacto@gmail.com>
2017-10-12 18:04:54 +0200

    Mejorada jerarquia de carpetas

--- Ejercicios de Programacion/HolaMundo (otra copia).java
+++
@@ -1,9 +0,0 @@
-/* Hola mundo! Muestro en pantalla el mensaje "Hola me llamo Salva!"
- */
- 
-
-public class HolaMundo {
-  public static void main(String[] args) {
-    System.out.print ("Hola me llamo Salva!");
-  }
-}
