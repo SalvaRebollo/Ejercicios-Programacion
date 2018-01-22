9d56eb57e9a67ad3b5e36634547a6a34f1fcfdcb
Salvador Rebollo Benítez <salvirbcontacto@gmail.com>
2018-01-22 09:46:35 +0100

    Añadiendo ejercicios Tema 8 y host SalvaRebollo.github.io

--- Ejercicios de Programacion/Tema8_Funciones/Ejercicios1-14/pruebaFunciones.java
+++ Ejercicios de Programacion/Tema8_Funciones/Ejercicios1-14/pruebaFunciones.java
@@ -2,7 +2,6 @@ import salvi.Funciones;
 
 public class pruebaFunciones {
 public static void main(String[] args) {
-  
   int numero;
   int x;
   int y;
@@ -12,62 +11,88 @@ public static void main(String[] args) {
   numero = Integer.parseInt(System.console().readLine());
   Funciones.voltea(numero);
   if (Funciones.esCapicua(numero) == true) {
-    System.out.println("Su número volteado es capicua. ");
+    System.out.println("Su número volteado es capicua.\n");
   } else {
-    System.out.println("Su número volteado NO es capicua.");
+    System.out.println("Su número volteado NO es capicua.\n");
   }
   
   //Funcion ejercicio 02 - esPrimo: Devuelve verdadero si el número que se pasa como parámetro es primo y falso en caso contrario.
   System.out.print("Funcion 02 esPrimo-Introduzca un numero entero positivo para saber si es primo: ");
   numero = Integer.parseInt(System.console().readLine());
-  System.out.println("¿Es " + numero + " primo?: " + salvi.Funciones.esPrimo(numero));
+  System.out.println("¿Es " + numero + " primo?: " + salvi.Funciones.esPrimo(numero) + "\n");
   
   //Funcion ejercicio 03 - siguientePrimo: Devuelve el menor primo que es mayor al número que se pasa como parámetro.
-  System.out.println("Funcion 03 siguientePrimo: Introduzca un numero: ");
+  System.out.print("Funcion 03 siguientePrimo: Introduzca un numero: ");
   numero = Integer.parseInt(System.console().readLine());
-  System.out.println("El siguiente numero primo despues de " + numero + " es: " + salvi.Funciones.siguientePrimo(numero));
+  System.out.println("El siguiente numero primo despues de " + numero + " es: " + salvi.Funciones.siguientePrimo(numero) + "\n");
   
   //Funcion ejercicio 04 - potencia: Dada una base y un exponente devuelve la potencia.
-  System.out.println("Funcion 04 potencia-Introduzca una base: ");
+  System.out.print("Funcion 04 potencia-Introduzca una base: ");
   int base = Integer.parseInt(System.console().readLine());
   System.out.println("Introduzca un exponente: ");
   int exponente = Integer.parseInt(System.console().readLine());
-  System.out.println(base + " elevado a " + exponente + " = " + salvi.Funciones.potencia(base, exponente));
+  System.out.println(base + "^" + exponente + " = " + salvi.Funciones.potencia(base, exponente) + "\n");
   
   //Funcion ejercicio 05 - digitos: Cuenta el número de dígitos de un número entero.
   System.out.print("Funcion 05 digitos-Introduzca un número entero positivo: ");
   numero = Integer.parseInt(System.console().readLine());
-  System.out.println(numero + " tiene " + salvi.Funciones.digitos(numero) + " dígitos.");
+  System.out.println(numero + " tiene " + salvi.Funciones.digitos(numero) + " dígitos.\n");
   
   //Funcion ejercicio 06 - voltea: Le da la vuelta a un número.
   System.out.print("Funcion 06 voltea-Introduce un número entero: ");
   numero = Integer.parseInt(System.console().readLine());
-  System.out.println("Su número volteado es: " + Funciones.voltea(numero));
+  System.out.println("Su número volteado es: " + Funciones.voltea(numero) + "\n");
   
   //Funcion ejercicio 07 - digitoN: Devuelve el dígito que está en la posición n de un número entero. Se empieza contando por el 0 y de izquierda a derecha
   System.out.print("Funcion 07 digitoN-Introduce un número entero: ");
   x = Integer.parseInt(System.console().readLine());
   System.out.println("Introduzca la posicion del numero a sacar: ");
   y = Integer.parseInt(System.console().readLine());
-  System.out.println("La cifra hayada en la posición " + y + " es: " + Funciones.digitoN(x, y));
+  System.out.println("La cifra hayada en la posición " + y + " es: " + Funciones.digitoN(x, y) + "\n");
   
   //Funcion ejercicio 08 - posicionDeDigito: Da la posición de la primera ocurrencia de un dígito dentro de un número entero. Si no se encuentra, devuelve -1.
-  
+  System.out.print("Funcion 08 posicionDeDigito-Introduce un número entero: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca el numero que quieres ver su posicion: ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("La posicion de la cifra " + y + " en el numero " + x + " es: " + Funciones.posicionDeDigito(x, y) + "\n");
   
   //Funcion ejercicio 09 - quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha).
-  
+  System.out.print("Funcion 09 quitaPorDetras-Introduce un número entero: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca los digitos a quitar por detras: ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("Resultado: " + Funciones.quitaPorDetras(x, y) + "\n");
   
   //Funcion ejercicio 10 - quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda). 
-  
+  System.out.print("Funcion 10 quitaPorDelante-Introduce un número entero: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca los digitos a quitar por delante: ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("Resultado: " + Funciones.quitaPorDelante(x, y) + "\n");
   
   //Funcion ejercicio 11 - pegaPorDetras: Añade un dígito a un número por detrás.
-  
+  System.out.print("Funcion 11 pegaPorDetras-Introduce un número entero: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca el dígito que se le va a pegar por la derecha ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("Resultado: " + Funciones.pegaPorDetras(x, y) + "\n");
   
   //Funcion ejercicio 12 - pegaPorDelante: Añade un dígito a un número por delante.
-  
+  System.out.print("Funcion 12 pegaPorDelante-Introduce un número entero: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca el dígito que se le va a pegar por la izquierda ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("Resultado: " + Funciones.pegaPorDelante(x, y) + "\n");
   
   //Funcion ejercicio 13 - trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un número y devuelve el trozo correspondiente.
-  
+  System.out.print("Funcion 13 trozoDeNumero-Introduce un número entero: ");
+  numero = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca la posicion inicial: ");
+  x = Integer.parseInt(System.console().readLine());
+  System.out.println("Introduzca la posicion final: ");
+  y = Integer.parseInt(System.console().readLine());
+  System.out.println("Resultado: " + Funciones.trozoDeNumero(numero, x, y) + "\n");
   
   //Funcion ejercicio 14 - juntaNumeros: Pega dos números para formar uno.
   System.out.println("Funcion 14 juntaNumeros-Introduzca el primer numero: ");
--- Ejercicios de Programacion/Tema8_Funciones/Ejercicios1-14/salvi/Funciones.java
+++ Ejercicios de Programacion/Tema8_Funciones/Ejercicios1-14/salvi/Funciones.java
@@ -13,9 +13,9 @@ public class Funciones{
  * @param x un número entero
  * @return Si el numero es capicua true/false
  */
-public static boolean esCapicua(int x) {
-  int copia = x;
-  int volteado = 0;
+public static boolean esCapicua(long x) {
+  long copia = x;
+  long volteado = 0;
   while (x > 0) {
     volteado = (volteado * 10) + (x % 10);
     x = x / 10;
@@ -26,6 +26,18 @@ public static boolean esCapicua(int x) {
     return false;
   }
 }
+
+ /**
+ * Funcion ejercicio 01 - esCapicua: Devuelve verdadero si el número que se pasa como parámetro es capicúa y falso en caso contrario.
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x un número entero
+ * @return Si el numero es capicua true/false
+ */
+public static boolean esCapicua(int x) {
+  return esCapicua((long)x);
+}
+
 
  /**
  * Funcion ejercicio 02 - esPrimo: Devuelve verdadero si el número que se pasa como parámetro 
@@ -87,7 +99,7 @@ public static int potencia (int x, int y) {
  * @param x un número entero
  * @return la cantidad de dígitos que contiene el número
  */
-public static int digitos(int x) {
+public static int digitos(long x) {
   if (x == 0) {
     return 1;
   } else {
@@ -124,7 +136,7 @@ public static long voltea(long x) {
  * @author Salvador Rebollo Benítez
  * @param x número entero
  * @param n posición dentro del número x
- * @return dígito que está en la posición n del número x empieza a contar por el 0 y de izquierda a derecha
+ * @return Dígito que está en la posición n de un número entero.
  */
   public static int digitoN(long x, int n) {
     x = voltea(x);
@@ -133,6 +145,135 @@ public static long voltea(long x) {
     }
    return (int)x % 10;
   }
+  
+ /**
+ * Funcion ejercicio 08 - posicionDeDigito: Da la posición de la primera ocurrencia 
+ * de un dígito dentro de un número entero. Si no se encuentra, devuelve -1.
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x Digito del que queremos saber una posicion
+ * @param d digito dentro del número x
+ * @return Posicion del numero d en el número x
+ */
+  public static int posicionDeDigito(long x, int d) {
+    int i;
+
+    for (i = 0; (i < digitos(x)) && (digitoN(x, i) != d); i++) {};
+
+    if (i == digitos(x)) {
+      return -1;
+    } else {
+      return i;
+    }
+  }
+  
+/**
+ * Funcion ejercicio 09 - quitaPorDetras: Le quita a un número n dígitos por detrás (por la derecha).
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x Digito del que queremos quitar por detras
+ * @param n digitos a quitar por detras
+ * @return Numero al le quitan n dígitos por detrás (por la derecha).
+ */
+  public static long quitaPorDetras(long x, int n) {
+    return x / (long)potencia(10, n);
+  }
+  
+/**
+ * Funcion ejercicio 10 - quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x Digito del que queremos quitar por delante
+ * @param n digitos a quitar por detras
+ * @return Numero al le quitan n dígitos por delante (por la izquierda).
+ */
+ public static long quitaPorDelante(long x, int n) {
+  x = pegaPorDetras(x, 1); // "cierra" el número por si acaso termina en 0
+  x = voltea(quitaPorDetras(voltea(x), n));
+  x = quitaPorDetras(x, 1);
+  return x;
+}
+
+/**
+ * Funcion ejercicio 10 - quitaPorDelante: Le quita a un número n dígitos por delante (por la izquierda).
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x Digito del que queremos quitar por delante
+ * @param n digitos a quitar por detras
+ * @return Numero al le quitan n dígitos por delante (por la izquierda).
+ */
+public static int quitaPorDelante(int x, int n) {
+  return (int)quitaPorDelante((long)x, n);
+}
+ /**
+ * Funcion ejercicio 11 - pegaPorDetras: Añade un dígito a un número por detrás (por la derecha).
+ * @param x número entero
+ * @param d dígito que se le va a pegar por la derecha
+ * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
+ *          por la derecha
+ */
+public static long pegaPorDetras(long x, int d) {
+  return juntaNumeros(x, d);
+}
+
+/**
+ * Funcion ejercicio 12 - pegaPorDelante: Añade un dígito a un número por delante.
+ *
+ * @param x número entero
+ * @param d dígito que se le va a pegar por la izquierda
+ * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
+ *          por la izquierda
+ */
+public static long pegaPorDelante(long x, int d) {
+  return juntaNumeros(d, x);
+}
+
+/**
+ * Funcion ejercicio 12 - pegaPorDelante: Añade un dígito a un número por delante.
+ *
+ * @param x número entero
+ * @param d dígito que se le va a pegar por la izquierda
+ * @return  número inicial <code>x</code> con el dígito <code>d</code> pegado
+ *          por la izquierda
+ */
+public static int pegaPorDelante(int x, int d) {
+  return (int)pegaPorDelante((long)x, d);
+}
+
+/**
+ * Funcion ejercicio 13 - trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un número y
+ * devuelve el trozo correspondiente.
+ * <p>
+ * Las posiciones se cuentan de izquierda a derecha comenzando por el cero.
+ *
+ * @param x      número entero
+ * @param inicio posición inicial
+ * @param fin    posición final
+ * @return       trozo de número compuesto por todos los dígitos que van desde
+ *               la posición inicial a la posición final incluyendo ambos
+ */
+public static long trozoDeNumero(long x, int inicio, int fin) {
+  int longitud = digitos(x);
+  x = quitaPorDelante(x, inicio);
+  x = quitaPorDetras(x, longitud - fin - 1);
+  return x;
+}
+
+/**
+ * Funcion ejercicio 13 - trozoDeNumero: Toma como parámetros las posiciones inicial y final dentro de un número y
+ * devuelve el trozo correspondiente.
+ * <p>
+ * Las posiciones se cuentan de izquierda a derecha comenzando por el cero.
+ *
+ * @param x      número entero
+ * @param inicio posición inicial
+ * @param fin    posición final
+ * @return       trozo de número compuesto por todos los dígitos que van desde
+ *               la posición inicial a la posición final incluyendo ambos
+ */
+public static int trozoDeNumero(int x, int inicio, int fin) {
+  return (int)trozoDeNumero((long)x, inicio, fin);
+}
 
 /**
  * Funcion ejercicio 14 - juntaNumeros: Pega dos numeros para formar uno
@@ -142,12 +283,22 @@ public static long voltea(long x) {
  * @param y Segundo numero
  * @return El resultante de juntar x e y
  */
-  public static int juntaNumeros (int x, int y) {
-    int longitud = Funciones.digitos(x);
-    x *= Funciones.potencia(10, longitud);
-    return x + y;
+  public static long juntaNumeros(long x, long y) {
+    return (long)(x * potencia(10, digitos(y))) + y;
   }
 
+
+/**
+ * Funcion ejercicio 14 - juntaNumeros: Pega dos numeros para formar uno
+ * 
+ * @author Salvador Rebollo Benítez
+ * @param x Primer numero
+ * @param y Segundo numero
+ * @return El resultante de juntar x e y
+ */
+  public static int juntaNumeros(int x, int y) {
+    return (int)(juntaNumeros((long)x, (long)y));
+  }
 
 }
 
