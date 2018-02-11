1d50ad7675c94dcb50b466a32c4257afb62111ff
SalvaRebollo <salvabenitez5@gmail.com>
2018-02-11 17:06:29 +0100

    Relacion POO actualizada

---
+++ Ejercicios de Programacion/Tema9_POO/2_POO_en_Java/Ejercicio01/Caballo.java
@@ -0,0 +1,39 @@
+package tema9POO.Ejercicio01;
+
+public class Caballo {
+  
+  private String nombre;
+  private String color;
+  private String raza;
+  private int edad;
+  private int carrerasGanadas;
+  private double peso;
+  
+  Caballo (String n, String c, int e, int cg) {
+    this.nombre = n;
+    this.color = c;
+    this.edad = e;
+    this.carrerasGanadas = cg;
+  }
+
+  public String getNombre() {
+    return this.nombre;
+  }
+  
+  public void come() {
+    System.out.println("*Estoy comiendo*");
+  }
+  
+  public void salta(){
+    System.out.println("Mira como salto");
+  }
+  
+  public void cabalga() {
+    System.out.println("*Estoy cabalgando*");
+  }
+  
+  public void relincha() {
+    System.out.println("*Relincho*");
+  }
+  
+}//class Caballo
---
+++ Ejercicios de Programacion/Tema9_POO/2_POO_en_Java/Ejercicio04/PruebaFraccion.java
@@ -0,0 +1,39 @@
+package tema9POO.Ejercicio04;
+/**
+ * Ejercicio 01: Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+ * instancias y aplicándole algunos métodos.
+ * 
+ * @author Salvador Rebollo Benitez
+ */
+public class PruebaFraccion {
+  public static void main(String[] args) {
+    
+    Fraccion a = new Fraccion(12,24);
+    Fraccion b = new Fraccion(15,30);
+    
+    System.out.println(a);
+    System.out.println(b);  
+    
+    System.out.println("Ahora multiplico la fraccion A por 5: ");
+    System.out.println(a + " * 5 = " + a.multiplica(5));
+    
+    System.out.println("Ahora multiplico la fraccion A por B: ");
+    System.out.println(a + " * " + b + " = " + a.multiplica(b));
+    
+    System.out.println("Ahora divido la fraccion A entre 5: ");
+    System.out.println(a + " / 5 = " + a.divide(5));
+    
+    System.out.println("Ahora divido la fraccion A entre B: ");
+    System.out.println(a + " / " + b + " = " + a.divide(b));
+    
+    System.out.println("Ahora simplifico la fraccion A: ");
+    System.out.println(a + " simplificada: " + a.simplifica());
+    
+    System.out.println("Ahora invierto A");
+    System.out.println(a + " invertida = " + a.invierte());
+    System.out.println("Ahora invierto B");
+    System.out.println(b + " invertida = " + b.invierte());
+    
+    
+  }
+}
